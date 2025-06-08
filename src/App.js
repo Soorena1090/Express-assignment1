70e9bcb87d5061266660c13dc3ec85a49980c9ca
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-06-08 22:09:55 +0330

    add validation

--- src/App.js
+++ src/App.js
@@ -4,40 +4,66 @@ const port = 3000;
 
 app.use(express.json());
 
-// app.get("/api/sum", (req, res) => {
-//   const a = +req.query.a;
-//   const b = +req.query.b;
-//   const result = a + b;
+app.get("/api/sum", (req, res) => {
+  const a = +req.query.a;
+  const b = +req.query.b;
+  const result = a + b;
 
-//   const response = {
-//     result: result,
-//   };
-//   res.send(response);
-// });
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(400).send({
+      error: "a and b must be numbers.",
+    });
+  }
 
-// app.post("/api/sort", (req, res) => {
-//   const numbers = req.body.numbers;
-//   const order = req.body.order;
-//   const orderNumbers = numbers.sort((a, b) => {
-//     if (order === "asc") {
-//       return a - b;
-//     } else if (order === "desc") {
-//       return b - a;
-//     } else {
-//       return a - b;
-//     }
-//   });
+  const response = {
+    result: result,
+  };
+  res.send(response);
+});
 
-//   res.send({
-//     result: orderNumbers,
-//   });
-// });
+app.post("/api/sort", (req, res) => {
+  const numbers = req.body.numbers;
+  const order = req.body.order;
+
+  if (isNaN(numbers)) {
+    return res.status(400).send({
+      error: "numbers must be number",
+    });
+  }
+  const orderNumbers = numbers.sort((a, b) => {
+    if (order === "asc") {
+      return a - b;
+    } else if (order === "desc") {
+      return b - a;
+    } else {
+      return res.status(400).send({
+        error: "please choose between 'asc' and 'desc' methods",
+      });
+    }
+  });
+
+  res.send({
+    result: orderNumbers,
+  });
+});
 
 app.post("/api/calculate", (req, res) => {
   const firstNumber = req.body.a;
   const secondNumber = req.body.b;
   const operation = req.body.operation;
   let result;
+
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    return res.status(400).send({
+      error: "Both 'a' and 'b' must be valid numbers.",
+    });
+  }
+
+  if (operation === "divide" && secondNumber === 0) {
+    return res.status(400).send({
+      error: "Divide by zero is not alowed.",
+    });
+  }
 
   switch (operation) {
     case "add":
