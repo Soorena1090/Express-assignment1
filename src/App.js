dae23c112a165f90dccc26b7ef594c230f53453f
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-06-08 20:29:04 +0330

    question 2 create post method for server

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/api/sum", (req, res) => {
   const a = +req.query.a;
   const b = +req.query.b;
@@ -12,6 +14,23 @@ app.get("/api/sum", (req, res) => {
   };
   res.send(response);
 });
+
+app.post('/api/sort',(req, res) => {
+    const numbers = req.body.numbers;
+    const order = req.body.order;
+    const orderNumbers = numbers.sort((a,b) => {
+        if(order === 'asc') {
+            return a - b;
+        }else if(order === 'desc') {
+            return b - a;
+        } else {
+            return a - b; 
+        }
+    });
+    res.send({
+        result: orderNumbers
+    })
+})
 
 app.listen(port, () => {
   console.log(`listen to port ${port}`);
