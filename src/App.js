f3873f9618389bb415d58bfeafab45422c7da017
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-06-08 20:52:02 +0330

    finish assignment with question 3 and make post method with switch case

--- src/App.js
+++ src/App.js
@@ -4,31 +4,60 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/api/sum", (req, res) => {
-  const a = +req.query.a;
-  const b = +req.query.b;
-  const result = a + b;
+// app.get("/api/sum", (req, res) => {
+//   const a = +req.query.a;
+//   const b = +req.query.b;
+//   const result = a + b;
 
-  const response = {
-    result: result,
-  };
-  res.send(response);
-});
+//   const response = {
+//     result: result,
+//   };
+//   res.send(response);
+// });
 
-app.post("/api/sort", (req, res) => {
-  const numbers = req.body.numbers;
-  const order = req.body.order;
-  const orderNumbers = numbers.sort((a, b) => {
-    if (order === "asc") {
-      return a - b;
-    } else if (order === "desc") {
-      return b - a;
-    } else {
-      return a - b;
-    }
-  });
+// app.post("/api/sort", (req, res) => {
+//   const numbers = req.body.numbers;
+//   const order = req.body.order;
+//   const orderNumbers = numbers.sort((a, b) => {
+//     if (order === "asc") {
+//       return a - b;
+//     } else if (order === "desc") {
+//       return b - a;
+//     } else {
+//       return a - b;
+//     }
+//   });
+
+//   res.send({
+//     result: orderNumbers,
+//   });
+// });
+
+app.post("/api/calculate", (req, res) => {
+  const firstNumber = req.body.a;
+  const secondNumber = req.body.b;
+  const operation = req.body.operation;
+  let result;
+
+  switch (operation) {
+    case "add":
+      result = firstNumber + secondNumber;
+      break;
+    case "subtract":
+      result = firstNumber - secondNumber;
+      break;
+    case "multiply":
+      result = firstNumber * secondNumber;
+      break;
+    case "divide":
+      result = firstNumber / secondNumber;
+      break;
+    default:
+      result = firstNumber + secondNumber;
+  }
+
   res.send({
-    result: orderNumbers,
+    result: result,
   });
 });
 
